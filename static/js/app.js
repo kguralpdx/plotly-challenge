6b9f41a83a0713eac159037e14b895a1efe58e03
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-10 17:28:52 -0700

    removed data.js reference in index.html, added init() function and then optionChanged function to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
-function buildDropdown() {
+function init() {
   // Get the data
-  d3.json("data/samples.json").then((data) => {
+  d3.json("./data/samples.json").then((data) => {
     console.log(data);
 
       // Grab values from the data json object to build the plots
@@ -13,24 +13,39 @@ function buildDropdown() {
   
       console.log(names);
 
-     // d3.select("#selDataset").selectAll("option").data()
+      // Build Test Subject dropdown
+      // d3.select("body").append("p").text("one").attr("id","p_1");
+      var dropdownList = d3.selectAll("#selDataset")
+        .selectAll("option")
+        .data(names)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+
+      // Populate Demographic Table
+
+
+
+
+
      // got help on this dropdown from https://www.aspsnippets.com/Articles/Populate-DropDownList-from-JSON-Array-using-JavaScript.aspx
-      var selectDatatset = document.getElementById("selDataset");
+      // var selectDatatset = document.getElementById("selDataset");
 
-      // Add option tags for each name in dataset
-      for (var i = 0; i < names.length; i++) {
-        var option = document.createElement("OPTION");
+      // // Add option tags for each name in dataset
+      // for (var i = 0; i < names.length; i++) {
+      //   var option = document.createElement("OPTION");
 
-        // Add the test subjects as the text
-        option.textContent = names[i];
-        console.log(option.textContent);
+      //   // Add the test subjects as the text
+      //   option.textContent = names[i];
+      //   console.log(option.textContent);
 
-        // Add the test subjects as the value
-        option.value = option.textContent;
-        console.log(option.value);
+      //   // Add the test subjects as the value
+      //   option.value = option.textContent;
+      //   console.log(option.value);
 
-        // Add the option tags
-        selectDatatset.options.add(option);
+      //   // Add the option tags
+      //   selectDatatset.options.add(option);
 
 
         // Bar plot
@@ -69,7 +84,7 @@ function buildDropdown() {
         
         // Plotly.newPlot('myDiv', data, layout);
 
-      };
+      });
 
 
 
@@ -113,8 +128,52 @@ function buildDropdown() {
   
     // // Plot the chart to a div tag with id "plot"
     // Plotly.newPlot("plot", data, layout);
-  })
+  //})
 };
 
-buildDropdown();
+function optionChanged() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var testSubject = dropdownMenu.property("value");
+      console.log(testSubject)
+          // Bar plot
+
+        // var data = [{
+        //   type: 'bar',
+        //   x: [20, 14, 23],
+        //   y: ['giraffes', 'orangutans', 'monkeys'],
+        //   orientation: 'h'
+        // }];
+        
+        // Plotly.restyle('myDiv', data);
+
+
+        // Bubble Plot
+
+        // var trace1 = {
+        //   x: [1, 2, 3, 4],
+        //   y: [10, 11, 12, 13],
+        //   text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+        //   mode: 'markers',
+        //   marker: {
+        //     color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        //     size: [40, 60, 80, 100]
+        //   }
+        // };
+        
+        // var data = [trace1];
+        
+        // var layout = {
+        //   title: 'Bubble Chart Hover Text',
+        //   showlegend: false,
+        //   height: 600,
+        //   width: 600
+        // };
+        
+        // Plotly.restyle('myDiv', data, layout);
+
+}
+
+init();
   
